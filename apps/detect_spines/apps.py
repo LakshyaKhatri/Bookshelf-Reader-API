4d6597fece47db71b7c456956b985561f06bef46
LakshyaKhatri <lakshyak67@gmail.com>
2019-03-27 19:57:58 +0530

    Add app for detecting spines.

---
+++ apps/detect_spines/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DetectSpinesConfig(AppConfig):
+    name = 'detect_spines'
--- bookshelf_reader/settings.py
+++ bookshelf_reader/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Add apps folder to the path.
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -38,9 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # django apps
+    'detect_spines',
+
+    # third party modules
     'rest_framework',
     'cv2',
-    'numpy'
+    'numpy',
 ]
 
 MIDDLEWARE = [
