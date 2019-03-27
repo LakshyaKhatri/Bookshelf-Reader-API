aaefbf9c3d336df3cd3c063f4a93a29b57d1d99e
LakshyaKhatri <lakshyak67@gmail.com>
2019-03-27 20:10:55 +0530

    Add template directory

--- bookshelf_reader/settings.py
+++ bookshelf_reader/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'bookshelf_reader.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_cdn")
+]
 # Settings for Django Rest Framework
 
 REST_FRAMEWORK = {
