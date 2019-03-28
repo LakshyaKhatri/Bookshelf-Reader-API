2be28a3293843aaa1ca77409d190409c45c3200c
LakshyaKhatri <lakshyak67@gmail.com>
2019-03-28 23:43:33 +0530

    Remove re_path from urls.py of apps, no usr right now

--- bookshelf_reader/urls.py
+++ bookshelf_reader/urls.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
