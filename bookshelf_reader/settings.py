63904416d71f4197e3926370a4048607102f2fea
LakshyaKhatri <lakshyak67@gmail.com>
2019-03-30 11:08:29 +0530

    Add 0.0.0.0 to allowed hosts for local testing

--- bookshelf_reader/settings.py
+++ bookshelf_reader/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'piuj%rb3u0)^utqd24(^wp)krvkdb8bzvt^dp8o#(fw)l1h*zc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', ]
 
 
 # Application definition
