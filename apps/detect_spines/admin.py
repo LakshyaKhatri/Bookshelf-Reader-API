a61907a6cbe03c2be7ad5a62cdc19674c220d323
LakshyaKhatri <lakshyak67@gmail.com>
2019-04-03 20:11:14 +0530

    Rename Book model to Bookshelf

--- apps/detect_spines/admin.py
+++ apps/detect_spines/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Book, Spine
+from .models import Bookshelf, Spine
 
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Bookshelf)
 admin.site.register(Spine)
--- apps/detect_spines/models.py
+++ apps/detect_spines/models.py
@@ -3,17 +3,17 @@ import os
 # Create your models here.
 
 
-def book_image_path(instance, filepath):
+def bookshelf_image_path(instance, filepath):
     base_name = os.path.basename(filepath)
     name, ext = os.path.splitext(base_name)
     print(name, ext)
-    new_filename = 'image'
+    new_filename = 'bookshelf'
     final_filname = "{new_filename}{ext}".format(
         new_filename=new_filename,
         ext=ext
     )
 
-    return "books_unseprated/{final_filname}".format(
+    return "bookshelf/{final_filname}".format(
         final_filname=final_filname
     )
 
@@ -33,8 +33,8 @@ def spine_image_path(instance, filepath):
     )
 
 
-class Book(models.Model):
-    image = models.ImageField(upload_to=book_image_path, null=True, blank=True)
+class Bookshelf(models.Model):
+    image = models.ImageField(upload_to=bookshelf_image_path, null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
@@ -42,10 +42,10 @@ class Book(models.Model):
 
 class Spine(models.Model):
     image = models.ImageField(upload_to=spine_image_path, null=True, blank=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    bookshelf = models.ForeignKey(Bookshelf, on_delete=models.CASCADE)
 
     def __str__(self):
         return "{book_id} : {spine_number}".format(
-            book_id=self.book.id,
+            book_id=self.bookshelf.id,
             spine_number=self.id
         )
