16641bf990d2ea2e061ecdcbe9f2b72381ce99a5
LakshyaKhatri <lakshyak67@gmail.com>
2019-04-03 18:50:02 +0530

    Add models for books and spines.

--- apps/detect_spines/admin.py
+++ apps/detect_spines/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Spine
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Spine)
--- apps/detect_spines/models.py
+++ apps/detect_spines/models.py
@@ -1,11 +1,33 @@
 from django.db import models
-
+import os
 # Create your models here.
 
 
+def upload_image_path(instance, filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    print(name, ext)
+    new_filename = 'image'
+    final_filname = "{new_filename}{ext}".format(
+        new_filename=new_filename,
+        ext=ext
+    )
+
+    return "books_unseprated/{final_filname}".format(
+        final_filname=final_filname
+    )
+
+
 class Book(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.CharField(max_length=200)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.id
+
+
+class Spine(models.Model):
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
