68f2d690678952abc410ef64ad4805e8318fedfb
LakshyaKhatri <lakshyak67@gmail.com>
2020-05-21 01:55:42 +0530

    Remove apps Folder from Project Path

--- bookshelf_reader/settings.py
+++ bookshelf_reader/settings.py
@@ -11,13 +11,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Add apps folder to the path.
-sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
